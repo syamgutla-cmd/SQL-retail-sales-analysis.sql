78df4aa8935afbb7b0e5bd78281b6fc363ee1f8e
syamgutla-cmd <syamgutla@gmail.com>
2026-07-05 01:30:45 +0530

    Update and rename README.md to retail_sales.sql

--- README.md
+++
@@ -1,1 +0,0 @@
-# SQL-retail-sales-analysis.sql
---
+++ retail_sales.sql
@@ -0,0 +1,40 @@
+# SQL-retail-sales-analysis.sql
+---create database
+CREATE DATABASE sql_project_p1;
+
+--create table
+DROP TABLE IF EXISTS retail_sales;
+CREATE TABLE retail_sales1(
+				transaction_id INT,
+				sale_date DATE,
+				sale_time TIME,
+				customer_id INT,
+				male VARCHAR(15),
+				age INT,
+				category VARCHAR(11),
+				quantiy INT,
+				price_per_unit FLOAT,
+				cogs FLOAT,
+				total_sale FLOAT
+
+);
+SELECT * FROM retail_sales
+LIMIT 10
+
+SELECT COUNT(*) FROM retail_sales
+
+
+ALTER TABLE retail_sales1
+RENAME TO retail_sales;
+
+ALTER TABLE retail_sales
+RENAME COLUMN male TO is_male;
+
+ALTER TABLE retail_sales
+ALTER COLUMN is_male TYPE BOOLEAN
+USING(
+      CASE 
+	     WHEN LOWER(is_male) = 'male' THEN true
+		 ELSE false
+      END
+);
