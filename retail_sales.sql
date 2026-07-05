9180735a839579749c08fdd969ef220521e5e501
syamgutla-cmd <syamgutla@gmail.com>
2026-07-05 22:59:29 +0530

    Update retail_sales.sql

--- retail_sales.sql
+++ retail_sales.sql
@@ -57,3 +57,17 @@ WHERE transaction_id IS NULL
    OR price_per_unit IS NULL
    OR cogs IS NULL
    OR total_sale IS NULL;
+
+--
+DELETE FROM retail_sales
+WHERE transaction_id IS NULL
+   OR sale_date IS NULL
+   OR sale_time IS NULL
+   OR customer_id IS NULL
+   OR is_male IS NULL
+   OR age IS NULL
+   OR category IS NULL
+   OR quantiy IS NULL
+   OR price_per_unit IS NULL
+   OR cogs IS NULL
+   OR total_sale IS NULL;
